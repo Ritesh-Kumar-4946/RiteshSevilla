745f7e55009dd56c47b0579131d09d0f00b5f1a3
ritesh <riteshkumar.4946@gmail.com>
2017-04-01 13:59:28 +0530

    Address Field Layout

--- app/src/main/java/com/ritesh/sevilla/DeliveryActivity.java
+++ app/src/main/java/com/ritesh/sevilla/DeliveryActivity.java
@@ -3,7 +3,8 @@ package com.ritesh.sevilla;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import butterknife.ButterKnife;
+import com.ritesh.sevilla.DeliverAddressPhoneFields.VerifyPhoneFragment;
+
 
 /**
  * Created by ritesh on 9/3/17.
@@ -15,7 +16,20 @@ public class DeliveryActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_delivery);
-        ButterKnife.bind(this);
+        setContentView(R.layout.activity_delivery_address_flags);
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, new VerifyPhoneFragment())
+                    .commit();
+        }
+
+
+        /*signup data is on VeryfyPhoneFragment.class*/
+        /*signup data is on VeryfyPhoneFragment.class*/
+        /*signup data is on VeryfyPhoneFragment.class*/
+        /*signup data is on VeryfyPhoneFragment.class*/
+        /*signup data is on VeryfyPhoneFragment.class*/
+        /*signup data is on VeryfyPhoneFragment.class*/
+        /*signup data is on VeryfyPhoneFragment.class*/
     }
 }
